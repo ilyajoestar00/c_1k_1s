c902c02b42d3fc2f27c6e63a1ab62a2f32e3c6a5
ilyajoestar00 <ilyajoestar00@gmail.com>
2026-09-25 22:56:05 +0300

    Initial commit

---
+++ lab01/lab01.1/src/main.c
@@ -0,0 +1,23 @@
+﻿// c_1k_1s.cpp: определяет точку входа для приложения.
+//
+
+#include "app.h"
+#include <math.h>
+#define SQR(x) (x)*(x)
+
+
+int main()
+{
+    double a = 5.21, b = 13.04, x = -8.07;
+
+
+    double t1 = log(fabs(x + sqrt(SQR(x) - SQR(a))));
+    
+    double t2 = b * (tan(x * 2));
+
+    double result = t1 / t2;
+
+    printf("Result: %.3f\n", result);
+    
+    return 0;
+}
---
+++ lab01/lab01.2/src/main.c
@@ -0,0 +1,31 @@
+﻿// c_1k_1s.cpp: определяет точку входа для приложения.
+//
+
+#include "app.h"
+#include <math.h>
+#define SQR(x) (x)*(x)
+
+
+int main()
+{
+    double a, b, x ;
+
+    printf("enter a, b, x:");
+
+	int r = scanf("%lf %lf %lf", &a, &b, &x);
+
+    if (r < 3) {
+		printf("Error: Invalid input. Please enter three numbers.\n");
+		return -1;
+	}
+
+    double t1 = log(fabs(x + sqrt(SQR(x) - SQR(a))));
+
+    double t2 = b * (tan(x * 2));
+
+    double result = t1 / t2;
+
+    printf("Result: %.3f\n", result);
+
+    return 0;
+}
